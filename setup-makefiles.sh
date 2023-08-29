9eeaed7ee3fc4a41eceb5c9f071172ba7b405af1
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:57:49 -0500

    fuxi: Add vendor soong imports to extract
    Change-Id: I7b290a9412bd11bb5577ce634a0291ffca40b283

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -5,6 +5,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function vendor_imports() {
+    cat << EOF >> "$1"
+		"device/xiaomi/sm8550-common",
+		"hardware/qcom-caf/sm8550",
+		"hardware/xiaomi",
+		"vendor/qcom/opensource/commonsys-intf/display",
+EOF
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
