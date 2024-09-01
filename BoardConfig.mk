f875d2b095960ab44e9ce9639e8046e5d104bbd0
Cosmin Tanislav <demonsingur@gmail.com>
2024-09-01 13:54:15 +0300

    fuxi: build common lineage powershare
    Change-Id: I378231ae12d8092187daa0e6e8ef8594b45363cf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,9 @@ BOARD_VENDOR_KERNEL_MODULES_LOAD += \
 # OTA
 TARGET_OTA_ASSERT_DEVICE := fuxi
 
+# Powershare
+TARGET_POWERSHARE_PATH := /sys/class/qcom-battery/reverse_chg_mode
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
